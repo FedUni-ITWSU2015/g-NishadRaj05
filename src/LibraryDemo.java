3821da8d3476553389bfe6feb548781a5cec362c
NishadRaj05 <nishadraj1809@gmail.com>
2025-08-26 23:40:37 +1000

    Creating LibraryDemo java file and one book and one patron in it.

---
+++ src/LibraryDemo.java
@@ -0,0 +1,10 @@
+
+public class LibraryDemo {
+
+	public static void main(String[] args) {
+		
+		Book book1 = new Book("The Hobbit", "J.R.R. Tolkien", "42492", true);
+        Patron patron1 = new Patron("Nishad Raj", "P001");
+	}
+
+}
