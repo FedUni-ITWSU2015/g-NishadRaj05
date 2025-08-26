e14fabd934c61d8451410d0a1eb16099251e0185
NishadRaj05 <nishadraj1809@gmail.com>
2025-08-26 22:27:28 +1000

    Creating Deposit method to add the specified amount to the account's balance

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,23 @@
 
 public class BankAccount {
-
+	
+	private double accountBalance;
+    private String accountNumber;
+    private String accountHolderName;
+    
+    public BankAccount(String number, String holderName, double initialBalance) {
+    	accountBalance = initialBalance;
+    	accountNumber = number;
+    	accountHolderName = holderName;
+    }
+    
+    public void deposit(double amount) {
+        if (amount > 0) {
+            accountBalance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+    
 }
