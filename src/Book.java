6b300dec448def09d5365158543c139e465c57a4
NishadRaj05 <nishadraj1809@gmail.com>
2025-08-26 23:05:10 +1000

    Constructor for book

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,15 @@
 
 public class Book {
-
+	
+	private String title;
+    private String author;
+    private String isbn;
+    private boolean available;
+    
+    public Book(String title, String author, String isbn, boolean available) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = available;
+    }
 }
