44921d359c6ee4bcda5770cb4717adaba2dbd19d
NishadRaj05 <nishadraj1809@gmail.com>
2025-08-26 22:47:02 +1000

    Creating new bank account then printing its information.

--- src/BankAccountDemo.java
+++ src/BankAccountDemo.java
@@ -2,8 +2,12 @@
 public class BankAccountDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		BankAccount acc = new BankAccount("78594", "Nishad Raj", 20000.0);
+		
+		System.out.println("Account Number: " + acc.getAccountNumber());
+        System.out.println("Account Holder: " + acc.getAccountHolderName());
+        System.out.println("Initial Balance: $" + acc.getBalance());
 	}
 
 }
