a4e133b86a53340df38dc110ceeb48666c1d3c84
NishadRaj05 <nishadraj1809@gmail.com>
2025-08-26 23:42:42 +1000

    Code to show the initial state of the book

--- src/LibraryDemo.java
+++ src/LibraryDemo.java
@@ -5,6 +5,9 @@ public class LibraryDemo {
 		
 		Book book1 = new Book("The Hobbit", "J.R.R. Tolkien", "42492", true);
         Patron patron1 = new Patron("Nishad Raj", "P001");
+        
+        System.out.println("Book: " + book1.getTitle() + " | Available: " + book1.getAvailable());
+        System.out.println("Patron: " + patron1.getName() + " | Books Borrowed: " + patron1.getNumberOfBooksBorrowed());
 	}
 
 }
