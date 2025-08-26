a844a7e20f6ab95ca6cef9f3729f0c792d0fa5c0
NishadRaj05 <nishadraj1809@gmail.com>
2025-08-26 19:41:08 +1000

    Declaring a new scanner then writing code in the main method that obtains input from the user for staff id, first name, surname, and address then creating a geek object by using the constructor with four arguments and Using the appropriate getter methods to display the state of the geek object after it has been created using the contructer

--- src/employeeDemo.java
+++ src/employeeDemo.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class employeeDemo {
 	public static void main(String[] args) {
@@ -5,7 +6,7 @@ public class employeeDemo {
 	     * This program demonstrates the use of the Employee class and some of its
 	     * methods including the two constructors
 	     */
-
+/*
 	    Employee person = new Employee(54545, "Bob", "Down", 7.5);
 	    System.out.println("Values for person object:");
 	    System.out.println("Staff ID: " + person.getStaffID());
@@ -17,8 +18,39 @@ public class employeeDemo {
 	    Employee accountant = new Employee();
 	    System.out.println("Staff ID: " + accountant.getStaffID());
 	    System.out.println("First Name: " + accountant.getFirstName());
+	    System.out.println("Surname: " + accountant.getSurname());
+	    System.out.println("Annual Leave: " + accountant.getAnnualLeave());
 	    System.out.println("Long Service Leave: " + accountant.getLongServiceLeave());
 	    System.out.println("Home Address: " + accountant.getAddress());
+	    */
+	    Scanner scan = new Scanner(System.in);
+	    
+        System.out.print("Let's create the geek object. Please enter the following data for the geek object.");
+	    
+        System.out.print("\nEnter staffID: ");
+        int ID = scan.nextInt();
+        
+        scan.nextLine();
+        
+        System.out.print("Enter First Name: ");
+        String fName = scan.nextLine();
+        
+        System.out.print("Enter Surname: ");
+        String sName = scan.nextLine();
+        
+        System.out.print("Enter address: ");
+        String addr = scan.nextLine();
+        
+        Employee geek = new Employee(ID, fName, sName, addr);
+        
+        System.out.println("\nWe just created the geek object. Let's check the value of it's data fields. ");
+        
+        System.out.println("StaffID: " + geek.getStaffID());
+        System.out.println("First Name: " + geek.getFirstName());
+        System.out.println("Surname: " + geek.getSurname());
+        System.out.println("Annual Leave: " + geek.getAnnualLeave());
+	    System.out.println("Long Service Leave: " + geek.getLongServiceLeave());
+        System.out.println("Address: " + geek.getAddress());
 
 	  }
 }
